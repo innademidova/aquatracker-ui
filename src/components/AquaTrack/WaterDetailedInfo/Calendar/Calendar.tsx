2592363df9fe23a1f27b39cd3be866fffc2e386e
innademidova <doc.idemidova@gmail.com>
2024-09-30 01:25:06 +0200

    changed Calendar using dayjs

--- src/components/AquaTrack/WaterDetailedInfo/Calendar/Calendar.tsx
+++ src/components/AquaTrack/WaterDetailedInfo/Calendar/Calendar.tsx
@@ -1,30 +1,20 @@
 import { useState } from 'react';
+import dayjs from 'dayjs';
 import Icon from '../../../Icon/Icon';
 import styles from './Calendar.module.scss';
-import { getDaysInMonth, getMonthName } from '../../../../utils/calendarHelper';
 
 const Calendar = () => {
-    const today = new Date();
-    const [currentMonth, setCurrentMonth] = useState(today.getMonth());
-    const [currentYear, setCurrentYear] = useState(today.getFullYear());
-    const daysInMonth = getDaysInMonth(currentYear, currentMonth);
+    const today = dayjs();
+    const [currentDate, setCurrentDate] = useState(today);
+
+    const daysInMonth = currentDate.daysInMonth();
 
     const handleNextMonth = () => {
-        if (currentMonth === 11) {
-            setCurrentMonth(0);
-            setCurrentYear(currentYear + 1);
-        } else {
-            setCurrentMonth(currentMonth + 1);
-        }
+        setCurrentDate(currentDate.add(1, 'month'));
     };
 
     const handlePreviousMonth = () => {
-        if (currentMonth === 0) {
-            setCurrentMonth(11);
-            setCurrentYear(currentYear - 1);
-        } else {
-            setCurrentMonth(currentMonth - 1);
-        }
+        setCurrentDate(currentDate.subtract(1, 'month'));
     };
 
     const renderDays = () => {
@@ -35,37 +25,40 @@ const Calendar = () => {
         return days;
     };
 
-    const currentMonthName = getMonthName(currentMonth, currentYear);
+    const currentMonthName = currentDate.format('MMMM');
+    const currentYear = currentDate.format('YYYY');
 
-    return <div className={styles.container}>
-        <div className={styles['calendar-header']}>
-            <span className={styles['calendar-title']}>Month</span>
-            <div className={styles['calendar-controls']}>
-                <div className={styles['month-navigation']}>
-                    <button onClick={handlePreviousMonth} className={styles['navigation-button']}>
-                        <Icon className={styles['icon']} glyph={'ArrowLeft'} />
-                    </button>
-                    <span className={styles['current-month']}>{`${currentMonthName}, ${currentYear}`}</span>
-                    <button onClick={handleNextMonth} className={styles['navigation-button']}>
-                        <Icon className={styles['icon']} glyph={'ArrowRight'} />
+    return (
+        <div className={styles.container}>
+            <div className={styles['calendar-header']}>
+                <span className={styles['calendar-title']}>Month</span>
+                <div className={styles['calendar-controls']}>
+                    <div className={styles['month-navigation']}>
+                        <button onClick={handlePreviousMonth} className={styles['navigation-button']}>
+                            <Icon className={styles['icon']} glyph={'ArrowLeft'} />
+                        </button>
+                        <span className={styles['current-month']}>{`${currentMonthName}, ${currentYear}`}</span>
+                        <button onClick={handleNextMonth} className={styles['navigation-button']}>
+                            <Icon className={styles['icon']} glyph={'ArrowRight'} />
+                        </button>
+                    </div>
+                    <button className={styles['statistic-button']}>
+                        <Icon className={styles['icon']} glyph={'Statistic'} />
                     </button>
                 </div>
-                <button className={styles['statistic-button']}>
-                    <Icon className={styles['icon']} glyph={'Statistic'} />
-                </button>
+            </div>
+            <div className={styles['calendar-grid']}>
+                {renderDays().map((day) => {
+                    return (
+                        <div className={styles['day-wrapper']} key={day}>
+                            <div className={styles.day}>{day}</div>
+                            <div className={styles['day-progress']}>100%</div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
-        <div className={styles['calendar-grid']}>
-            {renderDays().map((day) => {
-                return (
-                    <div className={styles['day-wrapper']} key={day}>
-                        <div className={styles.day}>{day}</div>
-                        <div className={styles['day-progress']}>100%</div>
-                    </div>
-                );
-            })}
-        </div>
-    </div>
-}
+    );
+};
 
 export default Calendar;
